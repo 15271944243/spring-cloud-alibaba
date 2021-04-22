0d0cb49915623dd6fe743d7631dfb67002c0cea2
xiaoxiaoxiang <xiaoxiaoxiang@xhqb.com>
2021-04-22 11:15:02 +0800

    add seata demo

---
+++ seata-client/README.md
@@ -0,0 +1,5 @@
+### Spring Cloud Alibaba 学习
+
+#### Seata Demo
+
+参考 https://github.com/seata/seata-samples
---
+++ seata-client/seata-order/src/main/java/sca/seata/client/order/SeataOrderApplication.java
@@ -0,0 +1,28 @@
+//package sca.seata.client.order;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.web.client.RestTemplate;
+//
+///**
+// * @description:
+// * @author: xiaoxiaoxiang
+// * @date: 2021/4/21 16:02
+// */
+//@SpringBootApplication
+//@EnableDiscoveryClient
+//public class SeataOrderApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(OrderClientApplication.class, args);
+//    }
+//
+//    @LoadBalanced
+//    @Bean
+//    public RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
+//}
